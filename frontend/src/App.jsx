8e3ccad199685b12091a1023768c529803c3b34e
deepaktripathi119 <deepaktripathi14022005@gmail.com>
2025-09-03 15:02:25 +0530

    reverted timeout app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { Routes, Route, Navigate, useLocation } from 'react-router-dom'; // Added useLocation
-import AuthPage from './components/AuthPage/AuthPage.jsx';
-import CreateAccount from './components/AuthPage/CreateAccount.jsx';
-import Verify from './components/AuthPage/Verify.jsx';
-import Dashboard from './components/DashBoardPage/DashBoardPage.jsx';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom"; // Added useLocation
+import AuthPage from "./components/AuthPage/AuthPage.jsx";
+import CreateAccount from "./components/AuthPage/CreateAccount.jsx";
+import Verify from "./components/AuthPage/Verify.jsx";
+import Dashboard from "./components/DashBoardPage/DashBoardPage.jsx";
+import axios from "axios";
 
 const App = () => {
   const [user, setUser] = useState(null);
@@ -13,11 +13,10 @@ const App = () => {
   const getUser = async () => {
     console.log("Fetching user data...");
     try {
-      const url = `${import.meta.env.VITE_REACT_APP_API_URL}/auth/login/success`;
-      const { data } = await axios.get(url, { 
-        withCredentials: true,
-        timeout: 120000 // Wait for 2 minutes
-      });
+      const url = `${
+        import.meta.env.VITE_REACT_APP_API_URL
+      }/auth/login/success`;
+      const { data } = await axios.get(url, { withCredentials: true });
       setUser(data.user);
     } catch (err) {
       setUser(null);
@@ -32,9 +31,21 @@ const App = () => {
 
   return (
     <Routes>
-      <Route exact path="/" element={user ? <Navigate to="/dashboard" /> : <AuthPage />} />
-      <Route exact path="/dashboard" element={user ? <Dashboard userData={user} /> : <Navigate to="/" />} />
-      <Route exact path="/signup" element={user ? <Navigate to="/dashboard" /> : <CreateAccount />} />
+      <Route
+        exact
+        path="/"
+        element={user ? <Navigate to="/dashboard" /> : <AuthPage />}
+      />
+      <Route
+        exact
+        path="/dashboard"
+        element={user ? <Dashboard userData={user} /> : <Navigate to="/" />}
+      />
+      <Route
+        exact
+        path="/signup"
+        element={user ? <Navigate to="/dashboard" /> : <CreateAccount />}
+      />
       <Route exact path="/verify" element={<Verify />} />
     </Routes>
   );
