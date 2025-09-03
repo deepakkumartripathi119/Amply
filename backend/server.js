b3d4b93b0d67f6505992fdaec05c1fd03d40f224
deepaktripathi119 <deepaktripathi14022005@gmail.com>
2025-09-03 16:46:23 +0530

    name issue fixed

--- backend/server.js
+++ backend/server.js
@@ -66,7 +66,7 @@ app.use(passport.session());
 
 
 const corsOptions = {
-    origin: ['https://amply-1.onrender.com','https://amply-liard.vercel.app', 'http://localhost:5173', 'http://localhost:5178'],
+    origin: ['https://amply-1.onrender.com', 'http://localhost:5173', 'http://localhost:5178'],
     methods: ['GET', 'POST', 'PUT'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true // Allow credentials (cookies, sessions)
